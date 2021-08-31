8267463bb6f1a4ee3bf3f912217627d16c00c932
bcalvin1 <bcalvin1@eecs.wsu.edu>
2021-08-30 23:23:32 -0700

    Finished

--- t.c
+++ t.c
@@ -15,11 +15,11 @@ int main(int argc, char *argv[ ], char *env[ ])
     // %%(1). Write C code to print values of argc and argv[] entries
 
     // %%ANSWER (1) 
-    printf("argv values: ");
+    printf("argc=%d\n", argc);
     for (int i=0; i<argc; i++) {
-        printf("%s ", argv[i]);
+        printf("argv[%d]=%s ", i, argv[i]);
     }
-    printf("\nargc value: %d\n", argc);
+    printf("\n");
 
     a=1; b=2; c=3;
     A(a,b);
@@ -33,7 +33,7 @@ int A(int x, int y)
     // %% write C code to PRINT ADDRESS OF d, e, f
 
     // %% ANSWER 
-    printf("d Address: %p\ne Address: %p\nf Address: %p\n", &d, &e, &f);
+    printf("&d=%8x &e=%8x &f=%8x\n", &d, &e, &f);
 
     d=4; e=5; f=6;
     B(d,e);
@@ -47,7 +47,7 @@ int B(int x, int y)
     // %% write C code to PRINT ADDRESS OF g,h,i
 
     // %% ANSWER 
-    printf("g Address: %p\nh Address: %p\ni Address: %p\n", &g, &h, &i);
+    printf("&g=%8x &h=%8x &i=%8x\n", &g, &h, &i);
 
     g=7; h=8; i=9;
     C(g,h);
@@ -62,7 +62,7 @@ int C(int x, int y)
         // %% write C cdoe to PRINT ADDRESS OF u,v,w,i,p;
 
         // %% ANSWER
-        printf("u Address: %p\nv Address: %p\nw Address: %p\ni Address: %p\np Address: %p\n", &u, &v, &w, &i, &p);
+        printf("&u=%8x &v=%8x &w=%8x &i=%8x &p=%8x\n", &u, &v, &w, &i, &p);
 
         u=10; v=11; w=12; i=13;
 
@@ -70,21 +70,25 @@ int C(int x, int y)
     // %% print FP value in HEX  
 
     // %% ANSWER 
-    printf("FP value in HEX: %X\n", FP);
+    printf("FP=%8x\n", FP);
 
 // (2). Write C code to print the stack frame link list.
-
     while (FP) {
-        printf("Stack Frame FP Value = %X\n", *FP);
+        printf("Stack Frame FP Value = %x\n", FP);
         FP = *FP;
     }
+    printf("Stack Frame FP Value = %x\n", FP);
 
     p = (int *)&p;
 
 // %%(3). Print the stack contents from p to the frame of main()
 //     YOU MAY JUST PRINT 128 entries of the stack contents.
 
-
+p = p-4;
+for (i=0; i<128; i++) {
+    printf("%x    %x\n", p, *p);
+    p++;
+}
 
 // %% (4). On a hard copy of the print out, identify the stack contents
 //     as LOCAL VARIABLES, PARAMETERS, stack frame pointer of each function.
